3f3ca8ffb54d5afaada2ed319fb59a8fcff5c0e0
Sergio Linero <SLineroDev@gmail.com>
2023-02-04 23:09:05 +0100

    Added Prettier and first files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+</script>
+
+<main>
+	<slot>loading...</slot>
+</main>
+
+<style>
+	main{
+		margin: auto;
+    padding: 1.5rem;
+		max-width: 60ch;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Posts from '../lib/components/Posts.svelte';
+	import Tabs from '../lib/components/Tabs.svelte';
+	import Topics from '../lib/components/Topics.svelte';
+	import type { TabItem } from '../lib/interfaces/tabs';
+
+	const tabs: TabItem[] = [
+		{ label: 'Topics', component: Topics },
+		{ label: 'Posts', component: Posts }
+	];
+</script>
+
+<Tabs {tabs} />
