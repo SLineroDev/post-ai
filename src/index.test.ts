bcfa28ee9983fff215b3e3bf779c7a8002f6874d
Sergio Linero <SLineroDev@gmail.com>
2023-02-07 13:16:42 +0100

    Format file

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,21 @@
-import type { GeneratePostSettings } from './lib/interfaces/post';
 import { getPostGeneratorPrompt, getTopicGeneratorPrompt } from '$lib/server';
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import type { GeneratePostSettings } from './lib/interfaces/post';
 
 describe('ideas promt generator', () => {
 	it('creates the correct prompt with the keyword love', () => {
+		const keyword: string[] = ['love'];
 
-		const keyword: string[] = ['love']
-
-		const expectedPromt = 'Generate 5 topic ideas for social media posts using the keywords "love".'
+		const expectedPromt =
+			'Generate 5 topic ideas for social media posts using the keywords "love".';
 
 		expect(getTopicGeneratorPrompt(keyword)).toBe(expectedPromt);
 	});
 	it('creates the correct prompt with the keywords programming, javascript, node', () => {
+		const keyword: string[] = ['programming', 'javascript', 'node'];
 
-		const keyword: string[] = ['programming', 'javascript', 'node']
-
-		const expectedPromt = 'Generate 5 topic ideas for social media posts using the keywords "programming", "javascript", "node".'
+		const expectedPromt =
+			'Generate 5 topic ideas for social media posts using the keywords "programming", "javascript", "node".';
 
 		expect(getTopicGeneratorPrompt(keyword)).toBe(expectedPromt);
 	});
@@ -23,29 +23,29 @@ describe('ideas promt generator', () => {
 
 describe('post promt generator', () => {
 	it('creates the correct prompt for a tweet', () => {
-
 		const settings: GeneratePostSettings = {
 			audience: 'everyone',
 			socialMedia: 'Twitter',
 			tone: 'Neutral',
 			title: 'How to become a better programmer'
-		}
+		};
 
-		const expectedPromt = 'Write a tweet using a Neutral tone about "How to become a better programmer".'
+		const expectedPromt =
+			'Write a tweet using a Neutral tone about "How to become a better programmer".';
 
 		expect(getPostGeneratorPrompt(settings)).toBe(expectedPromt);
 	});
 
 	it('creates the correct prompt for an Instagram post', () => {
-
 		const settings: GeneratePostSettings = {
 			audience: 'everyone',
 			socialMedia: 'Instagram',
 			tone: 'Formal',
 			title: 'How to become a better programmer'
-		}
+		};
 
-		const expectedPromt = 'Write a Instagram post using a Formal tone about "How to become a better programmer".'
+		const expectedPromt =
+			'Write a Instagram post using a Formal tone about "How to become a better programmer".';
 
 		expect(getPostGeneratorPrompt(settings)).toBe(expectedPromt);
 	});
